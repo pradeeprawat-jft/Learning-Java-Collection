ca2c2e5ecaa51e28e96c0b86e58892f71a7eb75e
pradeep rawat <pradeep.rawat@jellyfishtechnologies.com>
2023-03-22 11:24:52 +0530

    Day 22-03-23

---
+++ ListInterface.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class ListInterface {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Integer> v = new ArrayList<>();
+        System.out.println("Enter the length of array");
+        int size = input.nextInt();
+        System.out.println("Enter the Array Element");
+
+        for (int i = 0; i < size; i++) {
+            v.add(input.nextInt());
+        }
+
+        v.add(1, 20); // insert 20 at index 1
+        int length = v.size();  //get size of List
+        v.set(0, 10); // replace 10 at index 0
+        List<Integer> v2 = new ArrayList<>();
+        v2.add(40);
+        v2.add(50);
+        v2.add(60);
+        v.addAll(v2); // adding new arrayList(v2) to old arrayList(v)
+
+        System.out.println("element of index 3 is "+v.get(3)); // get element in index 3
+        v.sort(Collections.reverseOrder());
+        for (Integer i : v) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        Collections.sort(v);
+        for (Integer i : v) {
+            System.out.print(i + " ");
+        }
+    }
+}
